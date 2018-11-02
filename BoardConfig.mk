872ebcd6c747cbfc158c5f65e854fe635c982bd2
rahul1892 <kumar.rahul1892.gmail.com>
2018-11-02 22:04:05 +0530

    BoardConfig: Declare power and thermal HALs * Specifically delcare the power hal and thermal hal as NOS doesn't support pathmaps. * This commit should be reverted if building any other rom which supports pathmaps.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -213,6 +213,7 @@ TARGET_COPY_OUT_VENDOR := vendor
 
 # Power
 TARGET_USES_INTERACTION_BOOST := true
+TARGET_POWER_HAL := true
 
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/recovery.fstab
@@ -234,6 +235,9 @@ BOARD_SECCOMP_POLICY := $(DEVICE_PATH)/seccomp
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
+
+# Thermal
+TARGET_THERMAL_HAL := true
 
 # Treble
 BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
